7d917227dcd57cab411df7661d62b49246e453f2
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-10-13 13:38:33 -0700

    final commit

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import AppFunctional from './AppFunctional';
 
