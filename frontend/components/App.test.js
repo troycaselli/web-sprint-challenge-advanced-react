d14368857d61da5e5f371b8d051102dfa1a8c924
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-10-14 19:20:35 -0700

    attempt to fix custom test bug

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -14,10 +14,10 @@ test('function renders', () => {
 
 test('submit button renders message when valid email is entered', async () => {
   render(<AppFunctional />);
-  const emailInput = screen.getByRole('textbox');
+  // const emailInput = screen.getByRole('textbox');
   const submitButton = screen.getByRole('button', {name: /submit/i});
 
-  fireEvent.change(email, { target: { value: 'real@email.com' } })
+  fireEvent.change(screen.getByRole('textbox'), { target: { value: 'real@email.com' } })
   fireEvent.click(submitButton);
 
   const response = await screen.findByText(/real win #26/i);
@@ -26,16 +26,16 @@ test('submit button renders message when valid email is entered', async () => {
 
 test('submit button renders proper messages when invalid email input is provided', async () => {
   render(<AppFunctional />);
-    const emailInput = screen.getByRole('textbox');
+    // const emailInput = screen.getByRole('textbox');
     const submitButton = screen.getByRole('button', {name: /submit/i});
 
-    fireEvent.change(emailInput, { target: { value: 'fake@email' } })
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: 'fake@email' } })
     fireEvent.click(submitButton);
 
     const message = await screen.findByText(/Ouch: email must be a valid email/i);
     expect(message).toBeInTheDocument();
 
-    fireEvent.change(emailInput, { target: { value: 'foo@bar.baz' } })
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: 'foo@bar.baz' } })
     fireEvent.click(submitButton);
     await waitFor(() => {
       expect(message).toHaveTextContent(/foo@bar.baz failure #23/i);
